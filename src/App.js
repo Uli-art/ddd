e7e3bbe0094189f4c5bca860e6a1a940083cec13
группы стационара <st@itstep.lan>
2021-10-14 21:23:55 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,95 @@
-import React from "react"
-import MyButton from "./components/MyButton/MyButton"
-import MyButtonClass from "./components/MyButton/MyButtonClass";
-import Clock from "./components/Clock";
-import Counter from "./components/Counterrr";
+import React, { useState } from "react"
+import NavBar from "./components/NavBar"
 
-const buttonArray=['success','primary','grey','red','yellow']
 
 const App=() => {
-  return (
-    <div className="App">
-    <Clock/>
-      {buttonArray.map((btn)=>
-          <MyButton key={btn} className={btn}>
-              {btn}
-          </MyButton>
-      )}
-      
-      <MyButton className={'primary'}>
-        I am primary button
-      </MyButton>
-   
-      
-      <MyButtonClass>+</MyButtonClass>
-      <MyButtonClass>-</MyButtonClass>
+  const [showFormUser, setShowFormUser]=useState(false);
+  const [users, setUsers]=useState([
+    {
+      name:'John',
+      phone:'7788'
+    },
+    {
+      name:'Thanos',
+      phone:'50%'
+    },
+    
+  ]);
+  const [user, setUser]=useState(
+    {
+      name:'',
+      phone:'',
+    }
+    );
+  const onChange=(e)=>{
+    if (e.target.id=="name"){
+      setUser({...user, name: e.target.value});
+    }else{
+      setUser({...user, phone: e.target.value});
+    }
+  }
 
-        <Counter/>
+  const addUser=()=>{
+    setUsers([...users,user]);
+    setUser({
+      name:'',
+      phone:'',
+    });
+  }
+console.log(user);
+  return (
+   <div className="App">
+     <NavBar/>
+  
+    <div className="container">
+    <div className="row m-1">
+      <div className="col s4">
+        <a className="waves-effect waves-light btn"
+        onClick={()=>setShowFormUser(!showFormUser)}
+        >
+          Add user
+        </a>
+      </div>
+      <div className="col s8">
+        {showFormUser && 
+        <>
+        <div className="input-field col s6">
+          <i className="material-icons prefix">account_circle</i>
+          <input id="name" type="text" class="validate" value={user.name} placeholder="EnterName"
+           onChange={onChange}/>
+          <a id="Add" className="waves-effect waves-light btn m-1"
+            onClick={()=>addUser()}
+            >
+            Add</a>
+        </div>
+        <div className="input-field col s6">
+          <i className="material-icons prefix">phone</i>
+          <input id="phone" type="tel" class="validate" placeholder="EnterPhone" onChange={onChange}/>
+          <a className="waves-effect waves-light right btn m-1">Cancel</a>
+        </div>
+        </>}
+      </div>
     </div>
+    <table>
+    <thead>
+      <tr>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Delete</th>
+      </tr>
+    </thead>
+
+    <tbody>
+     { users&& users.map((user)=><tr>
+        <td>{user.name}</td>
+        <td>{user.phone}</td>
+        <td>DELETE</td>
+      </tr>
+      )}
+    </tbody>
+  </table>
+        
+  </div> </div>
   );
 }
 
