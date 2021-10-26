7ca9831603d98d2914ee943c3f143f3e3f0d9276
st <st@admin.com>
2021-10-26 12:29:08 -0600

    project

---
+++ src/components/DevicesItem.jsx
@@ -0,0 +1,23 @@
+export const devices = [
+    {
+        id:1,
+        name:'Samsung A50',
+        brand:'Samsung',
+        desctiption:'cool smartphone',
+        price:600,
+    },
+    {
+        id:2,
+        name:'Iphone 10',
+        brand:'Apple',
+        desctiption:'perfect smartphone',
+        price:3200,
+    },
+    {
+        id:3,
+        name:'Nokia 1',
+        brand:'Nokia',
+        desctiption:'nice smartphone',
+        price:7800,
+    },  
+]
